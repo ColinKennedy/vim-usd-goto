32025d5c32792884fbb898d3c6d9e0473edb8434
Colin Kennedy <colinvfx@gmail.com>
2019-06-15 13:44:32 -0700

    Updated usd_path_finder.py so that users can register their own 'resolver' functions

--- autoload/usd.vim
+++ autoload/usd.vim
@@ -1,14 +1,16 @@
-" This function had to be moved to autoload to avoid this issue with Vim commands
-" Reference: https://stackoverflow.com/a/22633702/3626104
-"
-function! usd#run_command_on_usd_file(command, mode)
+function! usd#resolve(path)
 pythonx << EOF
 import usd_path_finder
 import vim
 
-path = usd_path_finder.get_selected_path(vim.eval('a:mode'))
+path = usd_path_finder.resolve(vim.eval('a:path'))
 vim.command('let l:usd_file_path = "{path}"'.format(path=path))
 EOF
 
-    execute ':' . a:command . ' ' . l:usd_file_path
+    if l:usd_file_path == ''
+        echoerr 'No USD file could be found'
+        return ''
+    endif
+
+    return l:usd_file_path
 endfunction
--- pythonx/usd_path_finder.py
+++ pythonx/usd_path_finder.py
@@ -8,48 +8,33 @@ This module basically replaces Vim's `:normal gf` command.
 """
 
 # IMPORT STANDARD LIBRARIES
-import logging
-import os
-import re
+import itertools
 import subprocess
 
 # IMPORT THIRD-PARTY LIBRARIES
 import vim
 
 # IMPORT LOCAL LIBRARIES
-import rez_helper
 import shell_helper
 
-_ASSET_IDENTIFIER = re.compile(r"@(?P<path>[^@]+)@")
+
+REGISTERED_RESOLVERS = []
 
 
-def _eval(key, default=None):
+def _resolve_using_usd(path):
     try:
-        return vim.eval(key)
-    except Exception:
-        return default
+        from pxr import Ar
+    except ImportError:
+        return ""
+
+    return Ar.GetResolver().Resolve(path)
 
 
 def _resolve_with_subprocess(path):
-    """str: Try to use `usdresolve` to resolve the path."""
-
-    def _get_resolve_base_command():
-        if shell_helper.which("usdresolve"):
-            return ["usdresolve"]
-
-        packages = (_eval("g:usd_rez_package", default="USD"),)
-        package = rez_helper.package_chooser(packages)
-        if not package:
-            return []
-
-        # Try to use Rez to get a shell which has the `usdresolve` executable
-        return ["rez-env", package, "--", "usdresolve"]
-
-    base = _get_resolve_base_command()
-    if not base:
+    if not shell_helper.which("usdresolve"):
         return ""
 
-    command = base + [path]
+    command = ['usdresolve', path]
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (output, error) = process.communicate()
 
@@ -62,87 +47,18 @@ def _resolve_with_subprocess(path):
     return output.rstrip()
 
 
-def find_selection():
-    """str: Find the closest path in `@` quotes, if any can be found."""
-    row, column = vim.current.window.cursor
-    row -= 1  # Subtract a row to get the current row's index
-    line = vim.current.buffer[row]
+def register_resolver(resolver):
+    if not callable(resolver):
+        raise ValueError('resolver "{resolver}" must be a callable function.'
+                         ''.format(resolver=resolver))
 
-    for match in _ASSET_IDENTIFIER.finditer(line):
-        if match.start(0) <= column and match.end(0) >= column:
-            return match.group("path")
+    REGISTERED_RESOLVERS.append(resolver)
+
+
+def resolve(path):
+    for resolver in itertools.chain([_resolve_using_usd _resolve_with_subprocess], REGISTERED_RESOLVERS):
+        resolved_path = resolver(path)
+        if resolved_path:
+            return resolved_path
 
     return ""
-
-
-def get_visual_selection():
-    """str: Get the user's visual selection, if any."""
-    start = vim.current.buffer.mark("<")
-
-    if start is None:
-        return ""
-
-    end = vim.current.buffer.mark(">")
-
-    if end is None:
-        return ""
-
-    start_row, start_column = start
-    end_row, end_column = end
-    start_row -= 1
-    end_row -= 1
-
-    characters = []
-    for row in range(start_row, end_row + 1):
-        line = vim.current.buffer[row]
-
-        if row == start_row and row == end_row:
-            line = line[start_column : end_column + 1]
-        elif row == start_row:
-            line = line[start_column:]
-        elif row == end_row:
-            line = line[: end_column + 1]
-
-        characters.append(line)
-
-    return "\n".join(characters)
-
-
-def get_selected_path(visual_mode):
-    """Find the path that is under the cursor (using visual mode or by guessing).
-
-    If the user is not in visual mode, find the nearest text in wrapped
-    in `@` characters and use that, instead.
-
-    Returns:
-        str: The found path, if any.
-
-    """
-    try:
-        from pxr import Ar
-
-        _resolve = Ar.GetResolver().Resolve
-    except ImportError:
-        _resolve = _resolve_with_subprocess
-
-    selection = ""
-    if visual_mode:
-        selection = get_visual_selection()
-
-    if not selection:
-        selection = find_selection()
-
-    if not selection:
-        return ""
-
-    if os.path.isabs(selection):
-        return selection
-    else:
-        # Resolve a relative path into an absolute path, if needed
-        current_directory = vim.eval('expand("%:p:h")')
-        selection = os.path.normpath(os.path.join(current_directory, selection))
-
-    # Change a USD path URI into a file-path, if needed
-    selection = _resolve(selection)
-
-    return selection
